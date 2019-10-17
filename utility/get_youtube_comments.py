76fae00b68abed5e42607daa6618dfd163170985
Oyesh Mann Singh <oyeshsin@hotmail.com>
2019-10-17 19:19:39 -0400

    Added new list

--- utility/get_youtube_comments.py
+++ utility/get_youtube_comments.py
@@ -1,17 +1,15 @@
 import os
 
-urls= ['RxfWgvB-Zcc',
-'x5zHAu_UULI',
-'7aSSjVrkYmI',
-'QeZ2WYLJMmg',
-'l6SAoNM_b6U',
-'7I4HfcAnYzA',
-'J0CtOeLbIYk',
-'SjlZw4iVq2Y',
-'qKFQ57sHdD8',
-'zZ8Y5ncN3Po',
-'AQBk02D5PNE']
+urls= ['ATEiz1N58D0',
+'sQ45_FoSV0k',
+'1nXx-0EK-ns',
+'rCPPVS_dwlw',
+'OJWwMbLO0DA',
+'4jOmSfBLRlk',
+'xah2_pUBkIc',
+'FcLe08Rtz_M']
 
+        
 for url in urls:
     print(url,end=" ")
     os.system("youtube-comment-scraper -o "+url+".json -f json "+url)
