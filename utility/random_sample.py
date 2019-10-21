632298fa1d66469ef9e6856ed40cb183380837b9
Sandesh10 <timilsina.sandesh@gmail.com>
2019-10-21 18:58:23 -0400

    EOL character changed

--- utility/random_sample.py
+++ utility/random_sample.py
@@ -66,7 +66,7 @@ def write_txt(output_dir, file, sample_data):
 			# If sentence ender not found, then add it
 			# Because we need to split sentence in unitag.exe
             if plain_txt[-1] not in ['?', '!', '|', '।'] and plain_txt[-1][-1] not in ['?', '!', '|', '।']:
-                plain_txt.append('?')
+                plain_txt.append('।')
 				
             # Cite the paper
             # Removal of very short or lengthy comments
