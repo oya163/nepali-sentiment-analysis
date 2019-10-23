243ab0afd2b7ed849d742b5fff0d4f7931c18c6b
oya163 <oyeshsin@hotmail.com>
2019-10-23 12:42:22 -0400

    Now it will walk through all the files in directory

--- utility/text_collector.py
+++ utility/text_collector.py
@@ -6,34 +6,33 @@
 	Date - 10/17/2016
 	
 	How to run:
-		python text_collector.py -idir ..\data\youtube\sampled\txt -o output_file.txt
+		python utility/text_collector.py
 '''
 
 import os
 import argparse
 
-parser = argparse.ArgumentParser(add_help=True, description=('Unitag format to CoNLL Format Parser'))
-parser.add_argument('--input_dir', '-idir', metavar='PATH', help='Input path directory')
-parser.add_argument('--output_file', '-o', metavar='PATH', help='Output file')
+parser = argparse.ArgumentParser(add_help=True, description=('Text Collector Parser'))
+parser.add_argument('--input_dir', '-idir', default='./data/youtube/sampled/txt', metavar='PATH', help='Input path directory')
+parser.add_argument('--output_file', '-ofile', default='./data/corpus/youtube_corpus.txt', metavar='PATH', help='Output file')
 
 args = parser.parse_args()
 
 
 def main():
-	input_dir = args.input_dir
-	output_file = args.output_file
+    input_dir = args.input_dir
+    output_file = args.output_file
+    
+    with open(output_file, 'w', encoding='utf-8') as outfile:
+        for root, dirs, files in os.walk(input_dir):
+            for f in files:
+                filename = os.path.join(root, f)
+                i_f = open(filename, 'r', encoding='utf-16')
+                reader = i_f.readlines()
+                for row in reader:
+                    outfile.write(row)
 
-	with open(output_file, 'w', encoding='utf-8') as o_f:
-		for file in os.listdir(input_dir):
-			filename = os.path.join(input_dir, file)
-			i_f = open(filename, 'r', encoding='utf-16')
-			reader = i_f.readlines()
-			for row in reader:
-				o_f.write(row)
-	o_f.close()
 
-
-				
 if __name__ == "__main__":
 	main()
 
