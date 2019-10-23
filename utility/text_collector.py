f4e5c3d56ef72717fd20a7fcd7fc6d9780ed8901
oya163 <oyeshsin@hotmail.com>
2019-10-23 16:32:25 -0400

    Added unicode

--- utility/text_collector.py
+++ utility/text_collector.py
@@ -15,6 +15,7 @@ import argparse
 parser = argparse.ArgumentParser(add_help=True, description=('Text Collector Parser'))
 parser.add_argument('--input_dir', '-idir', default='./data/youtube/sampled/txt', metavar='PATH', help='Input path directory')
 parser.add_argument('--output_file', '-ofile', default='./data/corpus/youtube_corpus.txt', metavar='PATH', help='Output file')
+parser.add_argument('--unicode', '-u', default='utf16', choices=['utf8','utf16'], metavar='UTF', help='Encoding format')
 
 args = parser.parse_args()
 
@@ -22,12 +23,13 @@ args = parser.parse_args()
 def main():
     input_dir = args.input_dir
     output_file = args.output_file
+    encoding = args.unicode
     
     with open(output_file, 'w', encoding='utf-8') as outfile:
         for root, dirs, files in os.walk(input_dir):
             for f in files:
                 filename = os.path.join(root, f)
-                i_f = open(filename, 'r', encoding='utf-16')
+                i_f = open(filename, 'r', encoding=encoding)
                 reader = i_f.readlines()
                 for row in reader:
                     outfile.write(row)
