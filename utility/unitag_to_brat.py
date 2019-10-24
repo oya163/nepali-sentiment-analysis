76b718605f84b040db94fc0e3a4e266eb9d5ee89
oya163 <oyeshsin@hotmail.com>
2019-10-24 00:27:46 -0400

    Final touch up before proceeding into brat annotation

--- utility/unitag_to_brat.py
+++ utility/unitag_to_brat.py
@@ -1,12 +1,14 @@
 '''
 	Convert the output of unitag.exe
 	into CoNLL format file
+	Separates out text only and tag only
+	part into separate respective files
 	
 	Author - Oyesh Mann Singh
 	Date - 10/16/2016
 	
 	How to run:
-		python unitag_to_conll.py -idir ..\data\youtube\sampled\txt
+		python unitag_to_brat.py
 '''
 
 import csv
@@ -16,9 +18,12 @@ import argparse
 import shutil
 
 parser = argparse.ArgumentParser(add_help=True, description=('Unitag format to CoNLL Format Parser'))
-parser.add_argument('--input_dir', '-idir', metavar='PATH', help='Input path directory')
+parser.add_argument('--input_dir', '-idir', default='..\data\youtube\sampled\txt', metavar='PATH', help='Input path directory')
+parser.add_argument('--output_dir', '-odir', default='..\data\\final', metavar='PATH', help='Output path directory')
 
 args = parser.parse_args()
+
+output_dir = args.output_dir
 
 # Converts Unitag.exe output file
 # into CoNLL format file
@@ -61,8 +66,27 @@ def text_tag(inputfile, sent_file, tag_file):
 	txt_f.close()
 	tag_f.close()
 	print("Text tag file prepared !!!")
-				
-				
+
+
+# Copy files from old directory hierarchy
+# to new hierarchy before BRAT annotation
+def copyfiles():
+	for root, dirs, files in os.walk(args.input_dir):
+		for file in files:
+			filename = file.split('.')         # Splitting to get the middle name of file name
+			root_name = root.split('\\')       # Splitting root to get the last two directories
+			dir_name = root_name[-2:]          # Get the last two directory name
+			final_dir=os.path.join(output_dir, dir_name[0], dir_name[1])
+			if not os.path.exists(final_dir):
+				os.makedirs(final_dir)
+			if filename[1] == 'sent':
+				input_file = os.path.join(root, file)
+				print("Copying file", input_file)
+				out_file = os.path.join(final_dir, filename[0]+'.txt')
+				if os.path.exists(out_file):
+					os.remove(out_file)
+				shutil.copyfile(input_file, out_file)
+    
 def main():
 	for root, dirs, files in os.walk(args.input_dir):
 		for file in files:
@@ -74,14 +98,17 @@ def main():
 				# Converts Unitag format to CoNLL format 
 				print("Processing file : ", input_file)
 				converter(input_file, out_file)
-				
-				sent_file = os.path.join(root, filename[0]+'.sent.txt')
 				tag_file = os.path.join(root, filename[0]+'.tag.txt')
-				
+				sent_file = os.path.join(root, filename[0]+'.sent.txt')
+                
+
 				# Creates text_only and tag_only files
 				print("Processing file : ", out_file)
 				text_tag(out_file, sent_file, tag_file)
+                
+	print("Starting to copy files")
+	copyfiles()            
 
-				
 if __name__ == "__main__":
 	main()
+    
