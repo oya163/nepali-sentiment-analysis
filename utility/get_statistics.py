3c8c5639fa8b2c0b2c2eb66ad1c39f328145bcf1
Oyesh Mann Singh <oyeshsin@hotmail.com>
2020-02-06 11:05:02 -0500

    Get statistics from conll file

---
+++ utility/get_statistics.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+# Simple program to check data statistics of NER file
+# Input file should be in standard Stanford format
+# Outputs number of PER, LOC, ORG tags
+
+import csv
+import argparse
+import os
+from collections import Counter
+from tabulate import tabulate
+
+def main():
+	parser = argparse.ArgumentParser(description='Input file name')
+	
+	parser.add_argument('filename', metavar='STR',
+                    help='input valid file path')
+					
+	args = parser.parse_args()
+	
+	input_file = args.filename
+	
+	sent_counter = 0
+    
+	entities = {}
+	high_entities = {}    
+    
+	with open(input_file,'r', encoding='utf-8') as in_file:
+		reader = csv.reader(in_file, delimiter='\t')
+		for row in reader:
+			if row:
+				if row[0] not in entities:
+					entities[row[0]] = 1
+				else:
+					entities[row[0]] += 1
+                    
+				if row[0][2:] not in high_entities:
+					high_entities[row[0][2:]] = 1
+				else:
+					high_entities[row[0][2:]] += 1                    
+			else:
+				sent_counter += 1
+
+	print("***********Detailed statistics of entities***********")        
+	headers = ['Entities', 'Count']
+	data = sorted([(k,v) for k,v in entities.items()]) # flip the code and name and sort
+	print(tabulate(data, headers=headers))
+
+	print("***********High level statistics of entities***********")
+	data = sorted([(k,v) for k,v in high_entities.items()]) # flip the code and name and sort
+	print(tabulate(data, headers=headers))
+    
+	print("********************************************")
+	print("Total count of sentences = ", sent_counter)
+    
+    
+if __name__ == "__main__":
+	main()
