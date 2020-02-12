a7f31ed26d81b37643be227370fab202ae415717
oya163 <oyeshsin@hotmail.com>
2020-02-12 14:21:12 -0500

    Re-run

--- utility/word_vectors.py
+++ utility/word_vectors.py
@@ -13,12 +13,12 @@ import argparse
 from gensim.models import Word2Vec, KeyedVectors, FastText
 
 parser = argparse.ArgumentParser(add_help=True, description=('Text Collector Parser'))
-parser.add_argument('--input_dir', '-idir', default='../data/channels/comments', metavar='PATH', help='Input path directory')
-parser.add_argument('--output_dir', '-odir', default='../data/embeddings', metavar='PATH', help='Output path directory')
+parser.add_argument('--input_dir', '-idir', default='./data/channels/all_text', metavar='PATH', help='Input path directory')
+parser.add_argument('--output_dir', '-odir', default='./data/embeddings', metavar='PATH', help='Output path directory')
 parser.add_argument('--embeddings', '-e', default='fasttext', choices=['word2vec', 'fasttext'], metavar='STR', help='Embeddings')
 parser.add_argument('--embed_type', '-t', default=0, choices=[0, 1], metavar='INT', help='Embeddings Type 0: skip_gram, 1: cbow, default: 0}')
 parser.add_argument('--eval_mode', '-m', default=False, action="store_true", help='Evaluation mode')
-parser.add_argument('--similarity_check', '-s', default=['नेपाल', 'Nepal'], metavar='STR', help='Similarity check word')
+parser.add_argument('--similarity_check', '-s', default=['नेपाल',], metavar='STR', help='Similarity check word')
 
 
 args = parser.parse_args()
@@ -35,6 +35,9 @@ def main():
         output_dir = os.path.join(output_dir, 'nep2vec')
     else:
         output_dir = os.path.join(output_dir, 'nep2ft')
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
     
     output_file = os.path.join(output_dir, 'embeddings.vec')
 
@@ -45,6 +48,7 @@ def main():
         for root, dirs, files in os.walk(input_dir):
             for f in files:
                 input_file = os.path.join(root, f)
+                print("Processing {0}".format(input_file))
                 i_f = open(input_file, 'r', encoding='utf8')
                 for line in i_f:
                     if len(line) > 0:
@@ -54,7 +58,7 @@ def main():
             model = Word2Vec(sents, size=300, sg=embed_type, workers=10)
             
         elif embeddings == 'fasttext':            
-            model = FastText(size=4, window=5, min_count=1)
+            model = FastText(size=300, window=5, min_count=1)
             model.build_vocab(sentences=sents)
             total_examples = model.corpus_count
             model.train(sentences=sents, total_examples=total_examples, epochs=5)            
@@ -66,6 +70,7 @@ def main():
     
     print("Checking word similarity from: ", embeddings)
     for every in similarity_check:
+        print("Most similar words for ", every)
         print(model.most_similar(every, topn=10))
     
     # Print info
